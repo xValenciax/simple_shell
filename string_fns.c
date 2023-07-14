7eba7af5710456c0a0821865234bb7ec8ffd4875
Mahmoud Mohamed <71465891+xValenciax@users.noreply.github.com>
2023-07-14 19:35:48 +0300

    Update string_fns.c

--- string_fns.c
+++ string_fns.c
@@ -10,19 +10,19 @@
 
 int _atoi(char *s)
 {
-  unsigned int num = 0;
-  int sign = 1;
+unsigned int num = 0;
+int sign = 1;
 
-  do {
-    if (*s == '-')
-      sign *= -1;
-    else if (*s >= '0' && *s <= '9')
-      num = (num * 10) + (*s - '0');
-    else if (num > 0)
-      break;
-  } while(s++);
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+num = (num * 10) + (*s - '0');
+else if (num > 0)
+break;
+} while(s++);
 
-  return (num * sign);
+return (num * sign);
 }
 
 /**
@@ -88,5 +88,5 @@ return (dest);
  */
 int _putchar(char c)
 {
-	return (write(STDOUT_FILENO, &c, 1));
+return (write(STDOUT_FILENO, &c, 1));
 }
