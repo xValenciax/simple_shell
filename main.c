2d7d4f41189301d918d2f1951bf364f8967e157d
Mahmoud Mohamed <71465891+xValenciax@users.noreply.github.com>
2023-07-14 19:39:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ *
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: always 0
+ */
+
 int main(__attribute__((unused))int argc, char *argv[])
 {
 shell(argv[0]);
-return 0;
+return (0);
 }
 
